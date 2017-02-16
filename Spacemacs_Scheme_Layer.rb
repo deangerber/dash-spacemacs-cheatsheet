1b66b3b177a3f42e7f568854a844ccb60c224f89
Dean Gerber <dean.gerber@gmail.com>
2017-02-16 22:29:57 +0200

    feature: initial commit of scheme cheatsheet.

---
+++ Spacemacs_Scheme_Layer.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+cheatsheet do
+  title 'Spacemacs Scheme Layer'
+  docset_file_name 'SpacemacsSchemeLayer'
+  keyword 'spacemacs scheme'
+
+  introduction "Scheme language support for Spacemacs."
+
+  category do
+   id 'REPL'
+
+    entry do
+      command 'SPC m s i'
+      name 'Start or switch to the REPL'
+    end
+    entry do
+      command 'SPC m s s'
+      name 'Select Scheme implementation'
+    end
+    entry do
+      command 'SPC m s b'
+      name 'Send buffer to the REPL'
+    end
+  end
+end
